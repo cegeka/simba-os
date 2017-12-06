3b3a565000c34cb92d01aad81b5819175834b8fd
ventouristeam <ventouristeam@gmail.com>
2017-12-06 11:25:22 +0100

    timve: xavierl : added e-mail generation with Velocity

--- simba-core/src/main/java/org/simbasecurity/core/service/communication/ResetPasswordService.java
+++ simba-core/src/main/java/org/simbasecurity/core/service/communication/ResetPasswordService.java
@@ -1,19 +1,31 @@
 package org.simbasecurity.core.service.communication;
 
+import com.google.common.collect.ImmutableMap;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
 import org.simbasecurity.core.domain.User;
 import org.simbasecurity.core.domain.communication.token.Token;
 import org.simbasecurity.core.service.communication.mail.LinkGenerator;
+import org.simbasecurity.core.service.communication.mail.Mail;
 import org.simbasecurity.core.service.communication.mail.MailService;
+import org.simbasecurity.core.service.communication.mail.template.TemplateService;
 import org.simbasecurity.core.service.communication.token.TokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.StringWriter;
 import java.net.URL;
+
+import static org.simbasecurity.core.domain.user.EmailAddress.email;
+import static org.simbasecurity.core.service.communication.mail.Mail.mail;
 
 @Transactional
 @Service
 public class ResetPasswordService {
+
+    public static final String RESET_PASSWORD_SUBJECT = "reset password";
 
     @Autowired
     private MailService mailService;
@@ -21,11 +33,30 @@ public class ResetPasswordService {
     private TokenGenerator tokenGenerator;
     @Autowired
     private LinkGenerator linkGenerator;
+    @Autowired
+    private TemplateService templateService;
+
+    @Value("${simba.smtp.mail.from}")
+    private String resetPasswordFromAddress;
+    @Value("${simba.reset.password.mail.template}")
+    private String resetPasswordMailTemplate;
 
     public void sendMessage(User user) {
         Token token = tokenGenerator.generateToken(user);
         URL link = linkGenerator.generateResetPasswordLink(token);
-        mailService.sendMail(user.getEmail(), link);
+        mailService.sendMail(createMail(user, link));
+    }
+
+    private Mail createMail(User user, URL link) {
+        return mail()
+                .from(email(resetPasswordFromAddress))
+                .to(user.getEmail())
+                .subject(RESET_PASSWORD_SUBJECT)
+                .body(createBody(link.toString()));
+    }
+
+    private String createBody(String link){
+        return templateService.createMailBody(resetPasswordMailTemplate, ImmutableMap.of("link", link));
     }
 
 }
--- simba-core/src/main/java/org/simbasecurity/core/service/communication/mail/SmtpMailService.java
+++ simba-core/src/main/java/org/simbasecurity/core/service/communication/mail/SmtpMailService.java
@@ -1,41 +1,35 @@
 package org.simbasecurity.core.service.communication.mail;
 
-import org.simbasecurity.core.domain.user.EmailAddress;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.net.URL;
 import java.util.logging.Logger;
 
 public class SmtpMailService implements MailService {
 
     private static final Logger logger = Logger.getLogger(SmtpMailService.class.getName());
 
-    private String mailFromAddress;
     private JavaMailSender javaMailSender;
 
-    public SmtpMailService(JavaMailSender javaMailSender, String mailFromAddress) {
+    public SmtpMailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
-        this.mailFromAddress = mailFromAddress;
     }
 
     @Override
-    public void sendMail(EmailAddress email, URL url) {
+    public void sendMail(Mail mail) {
         MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         try {
-            mimeMessage.addRecipients(Message.RecipientType.TO, email.asString());
-            mimeMessage.addFrom(InternetAddress.parse(mailFromAddress));
-            mimeMessage.setSubject("Password reset");
-            mimeMessage.setText(url.toString());
+            mimeMessage.addRecipients(Message.RecipientType.TO, mail.getTo().asString());
+            mimeMessage.addFrom(InternetAddress.parse(mail.getFrom().asString()));
+            mimeMessage.setSubject(mail.getSubject());
+            mimeMessage.setContent(mail.getBody(), "text/html; charset=utf-8");
             javaMailSender.send(mimeMessage);
             logger.info("Reset password mail has been send");
         } catch (MessagingException e) {
-            throw new RuntimeException(String.format("Something went wrong when sending the mail to %s because %s", email, e.getCause()));
+            throw new RuntimeException(String.format("Something went wrong when sending mail %s because %s", mail, e.getCause()));
         }
     }
 }

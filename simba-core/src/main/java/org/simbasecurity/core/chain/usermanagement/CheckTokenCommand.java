a5673273e93522bb99e1ca5429e7eaf75c779d17
ventouristeam <ventouristeam@gmail.com>
2018-01-10 13:43:14 +0100

    bktis:brunol: extra checks on given email in password reset link

--- simba-core/src/main/java/org/simbasecurity/core/chain/usermanagement/CheckTokenCommand.java
+++ simba-core/src/main/java/org/simbasecurity/core/chain/usermanagement/CheckTokenCommand.java
@@ -4,6 +4,8 @@ import org.simbasecurity.core.chain.ChainContext;
 import org.simbasecurity.core.chain.Command;
 import org.simbasecurity.core.domain.User;
 import org.simbasecurity.core.domain.communication.token.Token;
+import org.simbasecurity.core.domain.user.EmailAddress;
+import org.simbasecurity.core.service.CredentialService;
 import org.simbasecurity.core.service.communication.token.UserTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,7 +13,6 @@ import org.springframework.stereotype.Component;
 import java.util.Optional;
 
 import static org.simbasecurity.core.chain.Command.State.CONTINUE;
-import static org.simbasecurity.core.chain.Command.State.ERROR;
 import static org.simbasecurity.core.chain.Command.State.FINISH;
 
 @Component
@@ -19,19 +20,34 @@ public class CheckTokenCommand implements Command {
 
     @Autowired
     private UserTokenService userTokenService;
+    @Autowired
+    private CredentialService credentialService;
 
     @Override
     public State execute(ChainContext context) throws Exception {
-        Optional<User> mayBeUser = context.getToken()
+        Optional<User> userFromToken = context.getToken()
                 .map(Token::fromString)
                 .flatMap(token -> userTokenService.getUserForToken(token));
-        if (mayBeUser.isPresent()) {
-            mayBeUser.ifPresent(user -> context.setUserName(user.getUserName()));
-            return CONTINUE;
-        } else {
+        Optional<User> userFromEmail =
+                context.getEmail().map(EmailAddress::email)
+                        .flatMap(email -> credentialService.findUserByMail(email));
+        if (!userFromEmail.isPresent()) {
+            //log with provided email
             context.redirectToWrongToken();
             return FINISH;
         }
+        if (!userFromToken.isPresent()){
+            //log with user of provided email
+            context.redirectToWrongToken();
+            return FINISH;
+        }
+        if (!userFromToken.get().equals(userFromEmail.get())){
+            //log with both users
+            context.redirectToWrongToken();
+            return FINISH;
+        }
+        userFromToken.ifPresent(user -> context.setUserName(user.getUserName()));
+        return CONTINUE;
     }
 
     @Override
--- simba-core/src/test/java/org/simbasecurity/core/chain/usermanagement/CheckTokenCommandTest.java
+++ simba-core/src/test/java/org/simbasecurity/core/chain/usermanagement/CheckTokenCommandTest.java
@@ -1,7 +1,5 @@
 package org.simbasecurity.core.chain.usermanagement;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,10 +10,13 @@ import org.simbasecurity.core.chain.Command;
 import org.simbasecurity.core.domain.User;
 import org.simbasecurity.core.domain.UserTestBuilder;
 import org.simbasecurity.core.domain.communication.token.Token;
+import org.simbasecurity.core.domain.user.EmailAddress;
+import org.simbasecurity.core.service.CredentialService;
 import org.simbasecurity.core.service.communication.token.UserTokenService;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.simbasecurity.core.chain.Command.State.CONTINUE;
@@ -29,6 +30,8 @@ public class CheckTokenCommandTest {
     private ChainContext chainContextMock;
     @Mock
     private UserTokenService userTokenServiceMock;
+    @Mock
+    private CredentialService credentialServiceMock;
 
     @InjectMocks
     private CheckTokenCommand checkTokenCommand;
@@ -40,7 +43,7 @@ public class CheckTokenCommandTest {
 
         Command.State state = checkTokenCommand.execute(chainContextMock);
 
-        Assertions.assertThat(state).isEqualTo(FINISH);
+        assertThat(state).isEqualTo(FINISH);
         verify(chainContextMock).redirectToWrongToken();
     }
 
@@ -50,19 +53,54 @@ public class CheckTokenCommandTest {
 
         Command.State state = checkTokenCommand.execute(chainContextMock);
 
-        Assertions.assertThat(state).isEqualTo(FINISH);
+        assertThat(state).isEqualTo(FINISH);
         verify(chainContextMock).redirectToWrongToken();
     }
 
     @Test
-    public void execute_withTokenInContextAndDatabase_statusContinue() throws Exception {
-        User user = UserTestBuilder.aDefaultUser().build();
-        when(chainContextMock.getToken()).thenReturn(Optional.of("sleutel!"));
+    public void execute_withTokenInContextAndDatabase_butUnknownEmailAddress_statusError() throws Exception {
+        User user = UserTestBuilder.aDefaultUser().withEmail("bruce@wayneindustries.com").build();
+        setupContextWith("bruce@wayneindustries.com", "sleutel!");
+
         when(userTokenServiceMock.getUserForToken(Token.fromString("sleutel!"))).thenReturn(Optional.of(user));
+        when(credentialServiceMock.findUserByMail(EmailAddress.email("bruce@wayneindustries.com"))).thenReturn(Optional.empty());
 
         Command.State state = checkTokenCommand.execute(chainContextMock);
 
-        verify(chainContextMock).setUserName(user.getUserName());
-        Assertions.assertThat(state).isEqualTo(CONTINUE);
+        assertThat(state).isEqualTo(FINISH);
+        verify(chainContextMock).redirectToWrongToken();
+    }
+
+    @Test
+    public void execute_withTokenInContextAndDatabase_butEmailAddressUserIsDifferentFromTokenUser_statusError() throws Exception {
+        User user = UserTestBuilder.aDefaultUser().withId(185L).withEmail("bruce@wayneindustries.com").build();
+        User snarf = UserTestBuilder.aDefaultUser().withId(665L).withEmail("snarf@lioncats.com").build();
+        setupContextWith("snarf@lioncats.com", "sleutel!");
+
+        when(userTokenServiceMock.getUserForToken(Token.fromString("sleutel!"))).thenReturn(Optional.of(user));
+        when(credentialServiceMock.findUserByMail(EmailAddress.email("snarf@lioncats.com"))).thenReturn(Optional.of(snarf));
+
+        Command.State state = checkTokenCommand.execute(chainContextMock);
+
+        assertThat(state).isEqualTo(FINISH);
+        verify(chainContextMock).redirectToWrongToken();
+    }
+
+    @Test
+    public void execute_withTokenInContextAndDatabase_EmailAddressUserIsSameAsTokenUser_statusContinue() throws Exception {
+        User user = UserTestBuilder.aDefaultUser().withEmail("bruce@wayneindustries.com").build();
+        setupContextWith("bruce@wayneindustries.com", "sleutel!");
+
+        when(userTokenServiceMock.getUserForToken(Token.fromString("sleutel!"))).thenReturn(Optional.of(user));
+        when(credentialServiceMock.findUserByMail(EmailAddress.email("bruce@wayneindustries.com"))).thenReturn(Optional.of(user));
+
+        Command.State state = checkTokenCommand.execute(chainContextMock);
+
+        assertThat(state).isEqualTo(CONTINUE);
+    }
+
+    private void setupContextWith(String email, String token) {
+        when(chainContextMock.getToken()).thenReturn(Optional.of(token));
+        when(chainContextMock.getEmail()).thenReturn(Optional.of(email));
     }
 }
